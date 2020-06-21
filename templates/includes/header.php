5ffd99cefeb805ca73d6bd29f685a780104239a9
shuoranz <shuoranzhang813@gmail.com>
2020-06-20 23:10:13 -0700

    update header

--- templates/includes/header.php
+++ templates/includes/header.php
@@ -74,71 +74,28 @@
 			<div id="mobnav-btn"></div>
 			<ul class="sf-menu">
 				<li class="normal_drop_down">
-				<a href="/#">Home</a>
-				<div class="mobnav-subarrow"></div>
-				<ul>
-					<li><a href="/index.html">Revolution version</a></li>
-					<li><a href="/index_2.html">Subscription version</a></li>
-					<li><a href="/index_3.html">With Ajax Search bar</a></li>
-                    <li><a href="/index_4.html">With Video</a></li>
-				</ul>
+					<a href="/">Home</a>
 				</li>
 				<li class="normal_drop_down">
-				<a href="/#">Course</a>
-				<div class="mobnav-subarrow"></div>
-				<ul>
-                	<li><a href="/courses_grid.html">Courses grid</a></li>
-                    <li><a href="/courses_list.html">Courses list</a></li>
-                    <li><a href="/course_detail_page_txt.html ">Course page Txt</a></li>
-                    <li><a href="/course_detail_page_video.html ">Course page Video</a></li>
-                    <li><a href="/course_details_4.html">Course details v1</a></li>
-                    <li><a href="/course_details_5.html">Course details v2</a></li>
-                    <li><a href="/course_details_2.html">Course details v3</a></li>
-					<li><a href="/course_details_1.html">Course details v4</a></li>
-                    <li><a href="/course_details_3.html">Course details v5</a></li>
-                    <li><a href="/course_timeline.html">Seminar timeline</a></li>
-				</ul>
+					<a href="/#">Online Courses</a>
+					<div class="mobnav-subarrow"></div>
+					<ul>
+						<li><a href="/course/?id=1">Mathematic</a></li>
+						<li><a href="/course/?id=2">Literature</a></li>
+						<li><a href="/course/?id=3">Physics</a></li>
+						<li><a href="/course/?id=4">Chemistry</a></li>
+						<li><a href="/course/?id=5">SAT Preparation</a></li>
+					</ul>
 				</li>
-				<li><a href="/prices_plans.html">Prices &amp; Plans</a></li>
-				<li><a href="/blog.html">Blog</a></li>
-                <li class="mega_drop_down">
-				<a href="/#">Pages (Megamenu)</a>
-                <div class="mobnav-subarrow"></div>
-                <div class="sf-mega">
-                	<div class="col-md-4 col-sm-6">
-                    	<h5>Communicate</h5>
-                            <ul class="mega_submenu">
-                            	<li><a href="/about_us.html">About us</a></li>
-								<li><a href="/teachers.html">Teachers</a></li>
-                                <li><a href="/teacher_profile.html">Teacher profile</a></li>
-                                <li><a href="/member_profile.html">Member profile</a></li>
-                                <li><a href="/blog.html">Blog</a></li>
-                                <li><a href="/events_news_list.html">News &amp; Events</a></li>
-                            </ul>
-                    </div>
-                  <div class="col-md-4 col-sm-6">
-                   <h5>Other pages</h5>
-                          <ul class="mega_submenu">
-                           	  <li><a href="/apply.html">PHP working wizard apply</a></li>
-                              <li><a href="/apply_2.html">PHP working apply</a></li>
-                              <li><a href="/login.html">Login</a></li>
-                              <li><a href="/register.html">Register</a></li>
-                              <li><a href="/shortcodes.html">Shortcodes</a></li>
-                              <li><a href="/gallery.html">Photo &amp; video gallery <img src="/img/new.png" alt=""></a></li>
-                          </ul>
-                    </div>
-                    <div class="col-md-4 col-sm-6">
-                    <h5>Submenu with icons</h5>
-                            <ul class="mega_submenu icons">
-                            	<li><a href="/#"> Downloads<i class="icon-download"></i></a></li>
-                                <li><a href="/#">Video files <i class="icon-video"></i></a></li>
-                                <li><a href="/#">Audio files <i class="icon-mic"></i></a></li>
-                                <li><a href="/#">Members <i class="icon-user"></i></a></li>
-                            </ul>
-                    </div>
-                </div>
-                </li>
-				<li><a href="/contacts.html">Contacts</a></li>
+				<li class="normal_drop_down">
+					<a href="/forum/">Fourm</a>
+				</li>
+				<li class="normal_drop_down">
+					<a href="/program/">Program Intro</a>
+				</li>
+				<li><a href="/blog/">Blog</a></li>
+				<li><a href="/news/">News</a></li>
+				<li><a href="/contact/">Contact Us</a></li>
 			</ul>
             
             <div class="col-md-3 pull-right hidden-sm hidden-xs">
