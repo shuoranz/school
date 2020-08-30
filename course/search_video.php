f48f40467330939b8f5adc7f880d17d105d879e3
shuoranz <shuoranzhang813@gmail.com>
2020-08-29 23:51:24 -0700

    reverse video search

--- course/search_video.php
+++ course/search_video.php
@@ -11,9 +11,11 @@ $user = new User;
 //Get Template and Assign Vars
 $template = new Template($pre_position.'templates/search_videos.php');
 
+/*
 if(!isset($_GET['p']) || (!is_numeric($_GET['p']) || strpos($_GET['p'], "."))) {
 	redirect("/course/search_video" . copyAndSetPageURI($_GET, 1), "invalid URL parameters","error");
 }
+*/
 
 //Assign Variables to template object
 
@@ -25,6 +27,11 @@ $template->totalUsers = $user->getTotalUsers();
 */
 $search_title = !isset($_GET["title"]) || $_GET["title"] == "" ? "" : $_GET["title"];
 $search_teacher =  !isset($_GET["teacher"]) || $_GET["teacher"] == "" ? "" : $_GET["teacher"];
+$template->videos = $course->searchVideosByAttribute($search_title, $search_teacher);
+
+
+
+/*
 $resultCount = count($course->searchVideosByAttribute($search_title, $search_teacher));
 $perPage = 7;
 $pages = pagination($_GET["p"], ceil($resultCount / $perPage), 5);
@@ -36,7 +43,7 @@ if ($_GET['p'] > ceil($resultCount / $perPage) && ceil($resultCount / $perPage) 
 $template->videos = $course->searchVideosByAttributeByPage($search_title, $search_teacher, $_GET["p"], $perPage);
 $template->pages = $pages;
 $template->pageMax = ceil($resultCount / $perPage);
-
+*/
 //Display template
 echo $template;
 
